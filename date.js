342bc24e7b4a8381b026ee3f2059d287d6a2b100
Aretm Grom <are@>
2024-10-13 22:54:09 +0300

    train from book

---
+++ date.js
@@ -0,0 +1,16 @@
+// // First way
+// const num1 = prompt();
+// const num2 = prompt();
+// const btn = document.getElementById("btn");
+// btn.onclick = function () {
+//   const summ = Number(num1) * Number(num2);
+//   console.log(summ);
+// };
+
+// // Second way
+// const num1 = prompt();
+// const num2 = prompt();
+// for (let i = 0; i == 0; i++) {
+//   summ = Number(num1) * Number(num2);
+//   console.log(summ);
+// }
